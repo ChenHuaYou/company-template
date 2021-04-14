e06a90791edd3e1ca456f6f2ee38250be1032f74
username <email>
2021-04-14 12:35:04 +0800

    hengda1.0

--- hengda/settings.py
+++ hengda/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*lh*yp@h_&*3n9cm)6+@@ol((^lt61^$=%g$*5lzi74dna5z4r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
